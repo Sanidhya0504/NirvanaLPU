2f269e951445c278a573e057e14190f5d3c51feb
Sanidhya <sharmasanidhya0504@gmail.com>
2022-05-19 19:51:00 +0530

    made about and contact

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,8 +1,57 @@
 import Nav from "./navbar.js";
+import Footer from "./footer.js";
+import Logo from "./logo_transp.png";
+import background from "./blank_bg.png";
+import "./about.css";
 function About() {
   return (
     <div className="App">
       <Nav></Nav>
+      <img className="nirvana_logo" src={Logo} />
+      <p className="about_text">
+        Your perfect event starts here. We believe in action — in doing before
+        saying. Live experiences, virtual events, and social impact actions —
+        connect to audiences through creative storytelling and thoughtful,
+        purpose-driven engagement.
+      </p>
+      <div
+        className="lead_banner"
+        style={{ backgroundImage: `url(${background})` }}
+      >
+        <br />
+        <h3>MEET OUR LEADRES</h3>
+        <br />
+
+        <img
+          className="lead_pic"
+          style={{ width: 275, height: 250, borderRadius: 200 }}
+          src={
+            "https://humornama.com/wp-content/uploads/2021/01/150-Rupiya-Dega-Meme-Template-of-Kachra-Seth-1024x576.jpg"
+          }
+        />
+        <br />
+        <h3>150 Rupiya Dega</h3>
+        <br />
+      </div>
+      <hr />
+      <h3>WHAT WE DO</h3>
+      <br />
+      <p className="about_text">
+        Through innovative design and flawless execution, we produce memorable
+        and unique events of all shapes, sizes and styles.
+      </p>
+
+      <h3>OUR CULTURE</h3>
+      <br />
+      <p className="about_text">
+        We do not need magic, we carry all the power we need inside ourselves
+        already: We have the power to imagine better. We aim to create a
+        diverse, equitable, and inclusive community to work in. We aspire to be
+        as environmentally conscious as possible in an industry that can be
+        wasteful. We strive to serve our communities and give our time and
+        resources. Our motto is simple: Celebrate. Live. Love. Laugh.
+      </p>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Components/contact.js
+++ src/Components/contact.js
@@ -4,6 +4,16 @@ function Contact() {
   return (
     <div className="App">
       <Nav></Nav>
+      <h1>Contact Now</h1>
+      <p>
+        We’re always in the market for new talent! Partners & suppliers, we
+        would love for you to join our database, so as future opportunities to
+        collaborate present themselves, we can be in touch.
+      </p>
+
+      <h3 className="contact_details">Email Us</h3>
+      <p>blahblahblah@blah.com</p>
+      <p>+91(IND) 1234567890</p>
       <Footer></Footer>
     </div>
   );
