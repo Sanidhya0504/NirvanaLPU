0566367cab1a9e7176820e82108bc9ddc0185163
Sanidhya <sharmasanidhya0504@gmail.com>
2022-05-19 18:25:28 +0530

    made card mobile responsive

--- src/Components/card.js
+++ src/Components/card.js
@@ -7,30 +7,30 @@ export class Card extends Component {
       <div className="card_wrap">
         <div className="container-fluid">
           <Carousel>
-            <Carousel.Item style={{ height: "300px" }}>
+            <Carousel.Item className="card_height" style={{ height: "300px" }}>
               <img
                 style={{ height: "300px" }}
-                className="d-block w-100"
+                className="d-block w-100 card_height"
                 src={"https://wallpaperaccess.com/full/2040735.jpg"}
               />
               <Carousel.Caption>
                 <h3>First Demo </h3>
               </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item style={{ height: "300px" }}>
+            <Carousel.Item className="card_height" style={{ height: "300px" }}>
               <img
                 style={{ height: "300px" }}
-                className="d-block w-100"
+                className="d-block w-100 card_height"
                 src={"https://wallpaperaccess.com/full/2040737.jpg"}
               />
               <Carousel.Caption>
                 <h3>Second Demo</h3>
               </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item style={{ height: "300px" }}>
+            <Carousel.Item className="card_height" style={{ height: "300px" }}>
               <img
                 style={{ height: "300px" }}
-                className="d-block w-100"
+                className="d-block w-100 card_height"
                 src={"https://wallpaperaccess.com/full/2040740.jpg"}
               />
               <Carousel.Caption>
